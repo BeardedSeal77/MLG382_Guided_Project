0e781628e948e0c5b0ebc422245cfa1aa17a6fac
BeardedSeal77 <64088921+BeardedSeal77@users.noreply.github.com>
2025-04-20 20:52:00 +0200

    Update dash 2.0

--- src/app.py
+++ src/app.py
@@ -1,5 +1,9 @@
 import dash
 
 app = dash.Dash(__name__)
-
 server = app.server
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8050))
+    app.run_server(debug = True, host = '0.0.0.0', port = port)
